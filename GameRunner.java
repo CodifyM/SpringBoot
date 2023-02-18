2a0e66241409035660b5c96115abe09c59769dea
CodifyM <120894178+CodifyM@users.noreply.github.com>
2023-02-18 20:14:11 +0530

    Create GameRunner.java

---
+++ GameRunner.java
@@ -0,0 +1,17 @@
+package com.in28minutes.spring.learnspringframework.game;
+
+public class GameRunner {
+	
+	private SuperContraGame game;
+	public GameRunner(SuperContraGame game) {
+		this.game = game;
+	}
+
+	public void run() {
+		game.up();
+		game.down();
+		game.left();
+		game.right();
+	}
+
+}
